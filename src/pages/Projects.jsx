b4dfa5afdce14f65bea4169833a295129f2e4908
MohamadRiza <rawufdeenriza@gmail.com>
2025-03-28 21:42:49 +0530

    maybe final update 3

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -27,7 +27,7 @@ const Projects = () => {
       image: "FoodieHub.png", // Replace with your actual image URL
       description:
         "FoodieHub is a MERN stack web application for online food ordering, developed as my final year project. It features Google authentication via Auth0, Stripe payments, and a modern UI with Tailwind CSS. Customers can search restaurants, place orders, and track statuses, while restaurant owners can manage menus and orders, providing a seamless and secure experience.",
-      technologies: ["MERN Stack", "TypeScript", "Tailwind CSS", "Shadcn UI", "Auth0 (Login/Sign up with Google) (API)", "Stripe Payment (API)", "RESTful API", "Postman/Thunder Client", "Git/Github", "VS Code"],
+      technologies: ["MERN Stack", "TypeScript", "Tailwind CSS", "Shadcn UI", "Auth0 (Login/Sign up with Google) (API)", "Stripe Payment (API)", "RESTful API", "Postman/Thunder Client", "Git/Github"],
       github: "https://github.com/MohamadRiza", // Replace with your actual GitHub link
     },
     {
